4718a27c102848558741f0ddcef68beebdbc26ea
Yuriy <mumrenkoyuriy@gmail.com>
2017-07-14 23:25:05 +0300

    some corrected

--- js/2.js
+++ js/2.js
@@ -6,20 +6,16 @@ let buttonTask1 = document.querySelector('.enterTask1');
 
 buttonTask1.addEventListener('click', howOld);
 
-
-function howOld(){
-	
-    if(inputYearOfBirth.value.length === 4){
-        alert('Ваш возвраст: ' + (2017 - inputYearOfBirth.value) + ' лет');
-    }
-    else if(inputYearOfBirth.value.length === 2){
-        alert('Ваш возвраст: ' + (17 - inputYearOfBirth.value));
-    }   
-    else{
-        alert('Введите корректную дату рождения');
-    } 
-
-}
+function howOld(){    
+	const now= new Date();    
+	const userDate = new Date((parseInt(inputYearOfBirth.value, 10)), 01, 01);    
+	if(inputYearOfBirth.value >= 0 && inputYearOfBirth.value < 18){
+		alert('Ваш возраст: ' + (now.getFullYear() - (2000 + parseInt(inputYearOfBirth.value, 10))));
+	}
+	else{
+		alert('Ваш возраст: ' + (now.getFullYear() - userDate.getFullYear()));
+	}
+} 
 
 //--------task 2 ----------------------------
 
@@ -30,8 +26,8 @@ let buttonTask2 = document.querySelector('.enterTask2');
 buttonTask2.addEventListener('click', maxNumber);
 
 function maxNumber(){
-    let result = Math.max(inputFirst.value, inputSecond.value);
-    alert('Максимальное число: ' + result );
+	let result = Math.max(inputFirst.value, inputSecond.value);
+	alert('Максимальное число: ' + result );
 
 }
 
@@ -42,16 +38,16 @@ let buttonTask3 = document.querySelector('.enterTask3');
 buttonTask3.addEventListener('click', numberPorch);
 
 function numberPorch(){
-    if(inputNumberFlat.value > 0 && inputNumberFlat.value< 21){
-        alert('Квартира находится в 1-м подьезде!')
-    }
-    else if(inputNumberFlat.value > 20 && inputNumberFlat.value< 65){
-        alert('Квартира находится во 2-м подьезде!')
-    }
-    else if(inputNumberFlat.value > 65 && inputNumberFlat.value< 81){
-        alert('Квартира находится в 3-м подьезде!')
-    }
-    else alert("Введите корректный номер квартиры");
+	if(inputNumberFlat.value > 0 && inputNumberFlat.value< 21){
+		alert('Квартира находится в 1-м подьезде!')
+	}
+	else if(inputNumberFlat.value > 20 && inputNumberFlat.value< 65){
+		alert('Квартира находится во 2-м подьезде!')
+	}
+	else if(inputNumberFlat.value > 64 && inputNumberFlat.value< 81){
+		alert('Квартира находится в 3-м подьезде!')
+	}
+	else alert("В этом доме квартир с таким номером нет!");
 }
 
 //--------task 4 ----------------------------
@@ -61,39 +57,28 @@ let pass = document.querySelector('.pass');
 let buttonTask4 = document.querySelector('.enterTask4');
 
 buttonTask4.addEventListener('click', isCorrect);
-
-function isCorrect(){    
-    if(login.value == 'ivan' && pass.value == '333' || login.value == 'ssss' && pass.value == '665' || login.value == 'gibs' && pass.value == '0000' ){
-       alert('Добро пожаловать');
-    }
-    else{        
-        alert('ошибка');        
-    }
-    login.value = '';
-    pass.value = '';
+let users = {
+	ivan: '333',
+	ssss: '665',
+	gibs: '0000'
 }
-
+function isCorrect(){    
+	users[login.value] === pass.value ? alert('Добро пожаловать') : alert('ошибка');
+	login.value = '';
+	pass.value = '';
+}
 //--------task 5 ----------------------------
-
-let inputNum = document.querySelectorAll('input');
+let inp1 = document.querySelector('.inp1');
+let inp2 = document.querySelector('.inp2');
+let inp3 = document.querySelector('.inp3');
 let buttonTask5 = document.querySelector('.enterTask5');
 
 buttonTask5.addEventListener('click', whatIsBiggest);
 
 function whatIsBiggest(){
-    let numberMax = 0;
-    for (let i = 0; i < inputNum.length; i++) {
-
-        if(inputNum[i].value > numberMax){
-            numberMax = inputNum[i].value;   
-             console.log ("inputNum =" + inputNum[i].value);          
-        }  
-        inputNum[i].value = '';  
-
-    }
-    alert('Максимальное число ' + numberMax);
+	let number = Math.max(inp1.value, inp2.value, inp3.value);
+	alert('Максимальное число ' + number);
 }
-
 
 //--------task 6 ----------------------------
 
@@ -114,21 +99,21 @@ function f(){
 //-------------------task 6.1 ---------------
 
 for (var i = 0; i < 102; i++) {
-    if(i % 2 == 0){
-        task1.textContent += i + ' ';
-    }    
+	if(i % 2 == 0){
+		task1.textContent += i + ' ';
+	}    
 }
 //-------------------task 6.2 ---------------
 
 for (var i = 200; i >= 0; i--) {    
-        task2.textContent += i + ' ';        
+	task2.textContent += i + ' ';        
 }
 
 //-------------------task 6.3 ---------------
 
 let number = 0;
 for (var i = 0; i < 101; i++) {    
-       number +=i;         
+	number +=i;         
 }
 
 task3.textContent += number;
@@ -138,18 +123,18 @@ let resMulti = 1;
 let multiplication = 7;
 
 for (let i=1; i<10; i++){
-resMulti = multiplication*i;
-task5.innerHTML+= i+ '* 7 = '+ resMulti +'<br>';
+	resMulti = multiplication*i;
+	task5.innerHTML+= i+ '* 7 = '+ resMulti +'<br>';
 }
 
 //-------------------task 6.6 ---------------
 let SpecialChar = '';
 for (var i = 1000; i <=2000; i++) {
-     
-    SpecialChar += String.fromCharCode(i);
-     
+	
+	SpecialChar += String.fromCharCode(i);
+	
 }
-        task6.textContent +=  SpecialChar + ' ';
+task6.textContent +=  SpecialChar + ' ';
 
 }
 
@@ -159,12 +144,12 @@ let buttonCleanUp = document.querySelector('.enterCleanUp');
 buttonCleanUp.addEventListener('click' , CleanUp);
 function CleanUp (){
 
-task1.textContent = '';
-task2.textContent = '';
-task3.textContent = '';
-task4.textContent = '';
-task5.textContent = '';
-task6.textContent = '';
+	task1.textContent = '';
+	task2.textContent = '';
+	task3.textContent = '';
+	task4.textContent = '';
+	task5.textContent = '';
+	task6.textContent = '';
 }
 //-------------------task 6.4 ---------------
 
@@ -175,11 +160,11 @@ let buttonTaskSix4 = document.querySelector('.enterTaskSix4');
 
 buttonTaskSix4.addEventListener('click', whatIsDegreeOf);
 function whatIsDegreeOf(){
-    let res = inpSixNum.value;
-    for (let i = 1; i < inpSixDegreeOf.value; i++) {
-        (res *= inpSixNum.value);
+	let res = inpSixNum.value;
+	for (let i = 1; i < inpSixDegreeOf.value; i++) {
+		(res *= inpSixNum.value);
 
-    }
+	}
     // alert('число ' + inpSixNum.value + ' в степени ' + inpSixDegreeOf.value + ' равно ' + res );
     task4.textContent+= 'Результат: '+res;
 
